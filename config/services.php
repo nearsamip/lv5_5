d5de24f2ca275d2082fb5da505820bc7121acec2
John Doe <nearsamip@gmail.com>
2017-10-15 11:06:00 +0545

    auth configuration and socialite configuration

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'facebook' => [
+        'client_id' => 'your-facebook-app-id',
+        'client_secret' => 'your-facebook-app-secret',
+        'redirect' => 'http://your-callback-url',
+    ],
+
 ];
